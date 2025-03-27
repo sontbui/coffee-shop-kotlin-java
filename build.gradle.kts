fad509150de7a5854674a8d5b2d8a2aca7cc3931
sontbui <sonbuithanh306@gmail.com>
2025-03-27 20:25:52 +0700

    Fix code

---
+++ build.gradle.kts
@@ -0,0 +1,6 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.google.services) apply false
+}
+// SHA1  32:61:37:0F:96:EF:2E:5D:A6:4A:CD:E1:A7:C0:2B:08:8E:7E:E6:92
